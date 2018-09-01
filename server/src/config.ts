41e626faf7ebffcd07f12a72e4ec439d839c6acc
Chad Engler <englercj@live.com>
2018-09-01 14:42:36 -0700

    cleanup config for using mysql on digital ocean

--- server/src/config.ts
+++ server/src/config.ts
@@ -7,7 +7,7 @@ export const isProductionEnv = env === 'production';
 export const isTestEnv = env === 'test';
 export const port = process.env.PORT || 8081;
 export const host = process.env.HOST || '127.0.0.1';
-export const region = process.env.REGION || 'us-east-2';
+export const region = process.env.REGION || 'nyc1';
 export const cors: corsMiddleware.Options = {
     origins: [isProductionEnv ? 'pixiplayground.com' : '*'],
     allowHeaders: [],
@@ -15,18 +15,17 @@ export const cors: corsMiddleware.Options = {
 };
 export const db: ISequelizeConfig = {
     operatorsAliases: false,
-    host: process.env.RDS_HOSTNAME || 'localhost',
-    port: process.env.RDS_PORT ? parseInt(process.env.RDS_PORT, 10) : 5432,
-    username: process.env.RDS_USERNAME || 'playground',
-    password: process.env.RDS_PASSWORD || '',
-    database: process.env.RDS_DB_NAME || 'pixi_playground',
-    dialect: isProductionEnv ? 'postgres' : 'sqlite',
+    host: process.env.DB_HOSTNAME || 'localhost',
+    port: process.env.DB_PORT ? parseInt(process.env.DB_PORT, 10) : 3306,
+    username: process.env.DB_USERNAME || 'playground',
+    password: process.env.DB_PASSWORD || '',
+    database: process.env.DB_NAME || 'pixi_playground',
+    dialect: isProductionEnv ? 'mysql' : 'sqlite',
     storage: isTestEnv ? ':memory:' : 'data.sqlite',
     define: {
         timestamps: true,
         version: 'lockVersion',
     },
-    // timezone: 'America/Los_Angeles',
-    benchmark: true,
-    native: true,
+    timezone: 'America/Los_Angeles',
+    benchmark: !isProductionEnv,
 };
